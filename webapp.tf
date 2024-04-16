4826d56be0465f84189a831f88e8fc7061e062ba
Shashikant Patil <shashikantpatil2727@outlook.com>
2024-04-16 11:55:06 +0530

    commit

--- webapp.tf
+++ webapp.tf
@@ -28,11 +28,26 @@ resource "azurerm_linux_web_app" "web_app" {
   name                = "example-my-app"
   location            = azurerm_resource_group.appservice-rg.location
   resource_group_name = azurerm_resource_group.appservice-rg.name
-  service_plan_id     = azurerm_service_plan.service-plan.id  # Corrected argument name
+  service_plan_id     = azurerm_service_plan.service-plan.id
 
   site_config {
-     
+    # Other site configurations can go here
+  }
+
+  app_settings = {
+    "JAVA_OPTS"             = "-Dserver.port=8080"  # Example Java options
+    "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"  # Example startup time limit in seconds
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
+
+     
+  
+
 
 
